9d95045a06e869ef2b8c4d6540336f2778d44baf
91qbj <91qbj0@gmail.com>
2026-08-13 19:07:28 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'ftbfifyxg.cc', 
+	'bidsbmjzl.com', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
