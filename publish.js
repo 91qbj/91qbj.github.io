baa944290867283f271e8bd15a57d5055808032c
91qbj <91qbj0@gmail.com>
2026-08-17 17:08:05 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'bidsbmjzl.com', 
+	'jdgzxafov.cc', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
