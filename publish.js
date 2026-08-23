e1c94dff5fcfc3ff08908a616d27b5cfc533d1cb
91qbj <91qbj0@gmail.com>
2026-08-24 04:14:19 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'xxheovub.cc', 
+	'wyzshbfe.com', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
