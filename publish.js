5ae41a229c8dc2a00d1f3dfacb56e788e7e216e0
91qbj <91qbj0@gmail.com>
2026-09-04 21:19:49 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'sssashcya.com', 
+	'umbsbgotu.cc', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
