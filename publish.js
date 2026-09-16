59656c2bbb4eb239760f0e7cdb50fc32075a1f81
91qbj <91qbj0@gmail.com>
2026-09-16 09:23:16 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'xkprcowh.com', 
+	'tpvllafj.com', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
