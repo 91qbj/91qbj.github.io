32801e007dea7c98f545f2570c917bdf31656578
91qbj <91qbj0@gmail.com>
2026-09-18 09:02:48 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'unamcjveb.cc', 
+	'bjtjbmoy.cc', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
