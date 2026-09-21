255bab84b5f8f224f60c04fa440d171ad158cb8f
91qbj <91qbj0@gmail.com>
2026-09-21 10:38:12 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'xizngred.cc', 
+	'uostyajr.com', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
