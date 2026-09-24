657ca8b35e9f226e3ceea69655b5ac320593a356
91qbj <91qbj0@gmail.com>
2026-09-24 16:39:42 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'udejeqtuo.cc', 
+	'rxonznctw.com', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
