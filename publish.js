a3472ae4e6bbe1f3b2560eabce29267bfd965993
91qbj <91qbj0@gmail.com>
2026-09-26 03:43:32 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'qekbbryx.com', 
+	'egmeiejvk.cc', 
 ];                                                                                                                  
 
 var JumpPage="91qbj.com";
